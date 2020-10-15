10d9d80552d834897383ea4ec92f0295e4a33e37
lekegitrepo <lekedevrepo@gmail.com>
2020-10-15 08:57:06 -0500

    remove the url and credential

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://foodlookupapp.netlify.app/'
+    origins '*'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
